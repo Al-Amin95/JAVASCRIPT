f06c0ba38daf16deaa2c60c4aee67fcfdfddbc36
Al-Amin95 <Al-Amin95>
2024-06-25 23:09:00 +0600

    js-basic

---
+++ practice.js
@@ -0,0 +1,34 @@
+
+document.getElementById('demo').innerHTML="BAHBVGSDJHABS";
+
+document.write(100+200);
+
+// window.alert("Danger");
+// alert("Danger");
+
+console.log(10+20);
+
+
+let x=5; //variable--value is changeable
+let y=6;
+let a=7.06897455;
+let st="aaaaa";
+// let z=x+y+ parseInt(a);
+let z=x+y+ parseFloat(a);
+document.getElementById('demo4').innerHTML=z.toFixed(2); 
+
+console.log(z);
+document.getElementById('demo3').innerHTML=st+10; 
+// document.getElementById('demo3').innerHTML=st*10; 
+// document.getElementById('demo3').innerHTML=st/10; 
+// document.getElementById('demo3').innerHTML=st-10; 
+
+
+ 
+
+document.getElementById('demo1').innerHTML=10; //literal-value is fixed
+document.getElementById('demo2').innerHTML=10.50; //literal-value is fixed
+
+
+
+
